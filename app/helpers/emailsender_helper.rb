8dbffbbc59c78c72f98d86e5adf7b43fb76a301d
root <root@tacite.quenee.ovh>
2015-11-14 14:06:49 +0100

    fix mail issue

--- app/helpers/emailsender_helper.rb
+++ app/helpers/emailsender_helper.rb
@@ -4,7 +4,7 @@ module EmailsenderHelper
   emails = contacts_email
   contact = Contact.find(booking.contact_id)
   message = "From: WebCalendar <noreply@" << Rails.configuration.x.email_smtp_domain << ">\n"
-  message << "To: " << emails << "\n"
+  message << "To: " << emails.to_s << "\n"
   message << "Subject: [" << Rails.configuration.x.name << "] Nouvelle réservation\n\n"
   message << "Ceci est un email automatique.\n"
   message << "Une nouvelle réservation a été effectuée du " << booking.start.to_s << " au " << booking.end.to_s << " pour " << booking.name << ".\n"
@@ -14,23 +14,33 @@ module EmailsenderHelper
   message << "Mot de passe: " << Rails.configuration.x.password 
 
   smtp = Net::SMTP.new(Rails.configuration.x.email_smtp_ip, Rails.configuration.x.email_smtp_port)
-  smtp.enable_starttls
-  smtp.start(Rails.configuration.x.email_smtp_domain, Rails.configuration.x.email_smtp_login, Rails.configuration.x.email_smtp_password, :login) do
+  # debug option
+  if Rails.configuration.x.email_debug
+    smtp.set_debug_output $stderr
+    puts "emails to:"
+    puts emails.to_s
+    puts "message to send:"
+    puts message
+  end
+  if Rails.configuration.x.email_smtp_ttls
+  	smtp.enable_starttls
+  else
+	smtp.disable_starttls
+  end
+  smtp.start(Rails.configuration.x.email_smtp_domain, Rails.configuration.x.email_smtp_login, Rails.configuration.x.email_smtp_password, Rails.configuration.x.email_authtype) do
   	smtp.send_message(message, "noreply@" << Rails.configuration.x.email_smtp_domain, emails)
   end
-  puts "email sent"
-  puts message
  end
 
  private
 
  def contacts_email
-  emails = ""
+  emails = []
   contacts = Contact.all
   contacts.each do |contact|
-    emails << contact.email << ","
+    emails.push(contact.email)
   end
-  return emails[0..(emails.length-1)]  
+  return emails
  end
 
 end
--- config/initializers/agenda.rb
+++ config/initializers/agenda.rb
@@ -1,16 +1,18 @@
 # Website settings
-Myapp::Application.config.x.website_url = "changeme.com"
-Myapp::Application.config.x.name = "Change Me"
-Myapp::Application.config.x.password = "password"
+Myapp::Application.config.x.website_url = ""
+Myapp::Application.config.x.name = "Saint Malo"
+Myapp::Application.config.x.password = ""
 Myapp::Application.config.x.maxroom = 15
 Myapp::Application.config.x.full_price = 12
 Myapp::Application.config.x.reduced_price = 5
 
-# Email settings
-Myapp::Application.config.x.email_smtp_domain = "home.com"
+# Email settings 
+Myapp::Application.config.x.email_smtp_domain = "gmail.com"
 Myapp::Application.config.x.email_smtp_ip = "smtp.gmail.com"
 Myapp::Application.config.x.email_smtp_port = 587
 Myapp::Application.config.x.email_smtp_ttls = true
+# value :login, :plain or :cram_md5
+Myapp::Application.config.x.email_authtype = :login
 Myapp::Application.config.x.email_smtp_login = "" 
-Myapp::Application.config.x.email_smtp_password = ""
- 
+Myapp::Application.config.x.email_smtp_password = "" 
+Myapp::Application.config.x.email_debug = false
