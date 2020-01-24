bc9ff887ac332f542138b4a7a8c1c3357ceb32e1
Joris QUENEE <joris.quenee@sap.com>
2020-01-24 18:28:21 +0100

    fix compilation issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   include SessionsHelper
   include BookingsHelper
-  include EmailsenderHelper
 
  def render_404
   render :file => "public/404.html",  :status => 404
